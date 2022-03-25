5c5af9b22df0c5ad95578caaefa01ee4456b2529
hshse <yakov.petryk@gmail.com>
2022-03-25 18:18:35 +0200

    start

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const cors = require('cors')
-const {Users, Desks} = require('./config')
-const PORT =  process.env.PORT || 3000
+const express = require("express");
+const cors = require("cors");
+const { Users, Desks } = require("./config");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
@@ -9,28 +9,28 @@ app.use(cors());
 
 app.post("/create", async (req, res) => {
   try {
-    const data = req.body
-    await Users.add(data)
-    await Desks.add(data)
+    const data = req.body;
+    await Users.add(data);
+    await Desks.add(data);
 
     res.send({ sended: "succesefull" });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 });
 
 app.get("/", async (req, res) => {
   try {
-   const data = await Users.get()
+    const data = await Users.get();
 
-    res.send({ users: data.docs.map(doc=>{ return {id:doc.id
-      ,...doc.data()
-    }})});
-
-  } catch( error) {
-      console.log(error)
+    res.send({
+      users: data.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() };
+      }),
+    });
+  } catch (error) {
+    console.log(error);
   }
-
 });
 
-app.listen(3000);
+app.listen(PORT);
