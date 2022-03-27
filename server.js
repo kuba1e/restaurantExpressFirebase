051dca424043442c9fa531115c9c450adb04cd5e
hshse <yakov.petryk@gmail.com>
2022-03-27 22:43:56 +0300

    added put, delete

--- server.js
+++ server.js
@@ -8,14 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//app.use(express.static(path.join(__dirname, "/restaurant/build")));
-
-/*
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/restaurant/build", "index.html"));
-});
-*/
-
 app.get("/waiters", async (req, res) => {
   try {
     const data = await Waiters.get();
@@ -26,8 +18,8 @@ app.get("/waiters", async (req, res) => {
     });
   } catch (error) {
     res.status(400).send({
-      message: "failed to get waiters"
-    })
+      message: "failed to get waiters",
+    });
   }
 });
 
@@ -41,24 +33,23 @@ app.get("/dishes", async (req, res) => {
     });
   } catch (error) {
     res.status(400).send({
-      message: "failed to get dishes"
-    })
+      message: "failed to get dishes",
+    });
   }
 });
 
 app.get("/tables", async (req, res) => {
   try {
     const data = await Tables.get();
-    res.send
-    ({
+    res.send({
       data: data.docs.map((doc) => {
         return { id: doc.id, ...doc.data() };
       }),
     });
   } catch (error) {
     res.status(400).json({
-      message: "failed to get tables"
-    })
+      message: "failed to get tables",
+    });
   }
 });
 
@@ -72,11 +63,10 @@ app.get("/bills", async (req, res) => {
     });
   } catch (error) {
     res.status(400).send({
-      message: "failed to get dishes"
-    })
+      message: "failed to get dishes",
+    });
   }
 });
-
 
 app.post("/tables", async (req, res) => {
   try {
@@ -89,5 +79,123 @@ app.post("/tables", async (req, res) => {
   }
 });
 
+app.post("/dishes", async (req, res) => {
+  try {
+    const data = req.body;
+    await Dishes.add(data);
+
+    res.send({ sended: "succesefull" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.post("/waiters", async (req, res) => {
+  try {
+    const data = req.body;
+    await Waiters.add(data);
+
+    res.send({ sended: "succesefull" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.post("/bills", async (req, res) => {
+  try {
+    const data = req.body;
+    await Bills.add(data);
+
+    res.send({ sended: "succesefull" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.put("/dishes/:id", async (req, res) => {
+  try {
+    const data = req.body;
+    const id = req.params.id;
+    await Dishes.doc(id).update({
+      ...data,
+    });
+
+    res.send({ updated: "succesefull" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.put("/waiters/:id", async (req, res) => {
+  try {
+    const data = req.body;
+    const id = req.params.id;
+    await Waiters.doc(id).update({
+      ...data,
+    });
+
+    res.send({ updated: "succesefull" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.put("/bills/:id", async (req, res) => {
+  try {
+    const data = req.body;
+    const id = req.params.id;
+    await Bills.doc(id).update({
+      ...data,
+    });
+
+    res.send({ updated: "succesefull" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.put("/tables/:id", async (req, res) => {
+  try {
+    const data = req.body;
+    const id = req.params.id;
+    await Tables.doc(id).update({
+      ...data,
+    });
+
+    res.send({ updated: "succesefull" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.delete("/dishes/:id", async (req, res) => {
+  try {
+    const data = req.body;
+    const id = req.params.id;
+    await Dishes.doc(id).delete();
+
+    res.send({ deleted: "succesefull" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.delete("/waiters/:id", async (req, res) => {
+  try {
+    const data = req.body;
+    const id = req.params.id;
+    await Waiters.doc(id).delete();
+
+    res.send({ deleted: "succesefull" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.delete("/bills/:id", async (req, res) => {
+  try {
+    const data = req.body;
+    const id = req.params.id;
+    await Bills.doc(id).delete();
+
+    res.send({ deleted: "succesefull" });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(PORT);
